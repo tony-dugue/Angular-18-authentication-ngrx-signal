dc8a672392324662ad279b6563bbd2f70c6b197d
Tony Dugué <duguetony@gmail.com>
2024-08-14 22:26:00 +0200

    feat: prepare home component with lazy route

--- projects/authentication-project/src/app/app.routes.ts
+++ projects/authentication-project/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home' },
+  //{ path: 'home', component: HomeComponent },
+  { path: 'home', loadComponent: () => import('./pages/home/home.component').then(item => item.HomeComponent) },
   { path: 'authenticate' },
   { path: '**', redirectTo: 'authenticate/login', pathMatch: 'full' },
 ];
