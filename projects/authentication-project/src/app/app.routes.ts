4e639f68a9b751e1ac685f6d3d67d6e04098b08f
Tony Dugué <duguetony@gmail.com>
2024-08-14 19:51:59 +0200

    feat: prepare routes

--- projects/authentication-project/src/app/app.routes.ts
+++ projects/authentication-project/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home' },
+  { path: 'authenticate' },
+  { path: '**', redirectTo: 'authenticate/login', pathMatch: 'full' },
+];
+
