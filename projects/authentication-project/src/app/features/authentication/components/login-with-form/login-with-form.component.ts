d0a7dcd0ccd30fcc929d82f4e7438d82fbd2559e
Tony Dugué <duguetony@gmail.com>
2024-08-15 23:54:43 +0200

    feat: create login form & submit

--- projects/authentication-project/src/app/features/authentication/components/login-with-form/login-with-form.component.ts
+++ projects/authentication-project/src/app/features/authentication/components/login-with-form/login-with-form.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { AuthenticationApplication } from '../../services/authentication.application';
 
 @Component({
   selector: 'app-login-with-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, MatInputModule, MatFormFieldModule, MatIconModule, MatButtonModule],
   templateUrl: './login-with-form.component.html',
   styleUrl: './login-with-form.component.css'
 })
 export class LoginWithFormComponent {
+  private readonly application = inject(AuthenticationApplication)
 
+  loginForm = inject(FormBuilder).group({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required)
+  })
+
+  save(): void {
+    if (this.loginForm.value.email && this.loginForm.value.password) {
+      this.application.login(this.loginForm.value.email, this.loginForm.value.password)
+    }
+  }
+
+  get email(): FormControl {
+    return this.loginForm.controls.email
+  }
+ 
+  get password(): FormControl {
+    return this.loginForm.controls.password
+  }
+
+  get isValid(): boolean {
+    return this.loginForm.valid
+  }
 }
